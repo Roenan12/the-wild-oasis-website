1519fc3dab0097715a3ab1a668fcddec176577fe
Roenan Halili <haliliroenan12@gmail.com>
2024-11-11 16:17:16 +0800

    refactor: fetching data in  a page

--- app/cabins/page.tsx
+++ app/cabins/page.tsx
@@ -1,7 +1,40 @@
-export default function Page() {
+type User = {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: {
+      lat: string;
+      lng: string;
+    };
+  };
+  phone: string;
+  website: string;
+  company: {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  };
+};
+
+export default async function Page(): Promise<JSX.Element> {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+
   return (
     <div>
       <h1>Cabins page</h1>
+
+      <ul>
+        {data.map((user: User) => (
+          <li key={user.id}>{user.name} </li>
+        ))}
+      </ul>
     </div>
   );
 }
