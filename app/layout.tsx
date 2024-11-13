24dfdb551165acc7374dbbbfbbcc9b3f3c5b4674
Roenan Halili <haliliroenan12@gmail.com>
2024-11-13 15:13:46 +0800

    refactor: improve navigation and root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
-import Logo from "@/app/_components/Logo";
-import Navigation from "@/app/_components/Navigation";
-
 import { Josefin_Sans } from "next/font/google";
+import "@/app/_styles/globals.css";
+import Header from "@/app/_components/Header";
 
 const josefin = Josefin_Sans({
   subsets: ["latin"],
@@ -10,8 +9,6 @@ const josefin = Josefin_Sans({
 });
 
 console.log(josefin);
-
-import "@/app/_styles/globals.css";
 
 export const metadata: Metadata = {
   // title: "The Wild Oasis",
@@ -30,13 +27,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={josefin.className}>
-      <body className="bg-primary-950 text-primary-100 min-h-screen ">
-        <header>
-          <Logo /> <Navigation />
-        </header>
-
-        <main>{children}</main>
-        <footer>Copyright by The Wild Oasis</footer>
+      <body className="antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col">
+        <Header />
+        <div className="flex-1 px-8 py-12">
+          <main className="max-w-7xl mx-auto">{children}</main>
+        </div>
       </body>
     </html>
   );
