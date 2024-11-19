1755b124545bcd97180b2cbfd838e762be979d6c
Roenan Halili <haliliroenan12@gmail.com>
2024-11-19 14:00:38 +0800

    feat(text-expander): add text expander in descriptions render a client component in server component

--- app/cabins/[cabinId]/page.tsx
+++ app/cabins/[cabinId]/page.tsx
@@ -1,3 +1,4 @@
+import TextExpander from "@/app/_components/TextExpander";
 import { Cabin, getCabin, getCabins } from "@/app/_lib/data-service";
 import { EyeSlashIcon, MapPinIcon, UsersIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
@@ -51,6 +52,7 @@ export default async function Page({ params }: { params: Params }) {
           <Image
             src={image ?? "/default-image.png"}
             className="object-cover"
+            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
             fill
             alt={`Cabin ${name}`}
           />
@@ -61,7 +63,11 @@ export default async function Page({ params }: { params: Params }) {
             Cabin {name}
           </h3>
 
-          <p className="text-lg text-primary-300 mb-10">{description}</p>
+          <p className="text-lg text-primary-300 mb-10">
+            <TextExpander>
+              {description ?? "No description available."}
+            </TextExpander>
+          </p>
 
           <ul className="flex flex-col gap-4 mb-7">
             <li className="flex gap-3 items-center">
