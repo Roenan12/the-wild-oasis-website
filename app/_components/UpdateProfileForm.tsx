a61139a6d876d61d3b4c792624d11323bcd35004
Roenan Halili <haliliroenan12@gmail.com>
2024-11-29 11:11:45 +0800

    refactor: extract button into new component

--- app/_components/UpdateProfileForm.tsx
+++ app/_components/UpdateProfileForm.tsx
@@ -3,7 +3,7 @@
 import React, { useState } from "react";
 import { Guest } from "../_lib/data-service";
 import { updateGuest } from "@/app/_lib/actions";
-import { useFormStatus } from "react-dom";
+import SubmitButton from "./SubmitButton";
 
 interface UpdateProfileFormProps {
   children: React.ReactNode;
@@ -64,21 +64,9 @@ function UpdateProfileForm({ children, guest }: UpdateProfileFormProps) {
       </div>
 
       <div className="flex justify-end items-center gap-6">
-        <Button />
+        <SubmitButton pendingLabel="Updating...">Update profile</SubmitButton>
       </div>
     </form>
-  );
-}
-// create a new component which needs to be rendered inside a form in order to use useFormStatus hook
-function Button() {
-  const { pending } = useFormStatus();
-  return (
-    <button
-      className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300"
-      disabled={pending}
-    >
-      {pending ? "updating..." : "Update profile"}
-    </button>
   );
 }
 
